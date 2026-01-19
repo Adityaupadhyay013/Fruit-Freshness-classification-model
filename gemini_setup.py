4bfa8200ae6c5dbc0f5df3a07d4979f095462268
Adityaupadhyay013 <adityaupadhyayusar@gmail.com>
2026-01-20 01:09:54 +0530

    Refactor Gemini setup and content generation logic

--- gemini_setup.py
+++ gemini_setup.py
@@ -1,13 +1,12 @@
 import google.generativeai as genai
 import os
 import re
-client = genai.Client(api_key = os.getenv(Gemini_api_key))
+genai.configure(api_key = os.getenv("Gemini_api_key"))
 def Gemini_ke_updesh():
     try:
-        response = client.models.generate_content(
-        model="gemini-3-flash-preview",
-        contents= "You are an fruit quality expert. Tell me tips for preventing fresh fruits from rotten. And effect of eating rotten fruits. Answer it in few lines only."
-        )
+        model=genai.generativemodel("gemini-3-flash-preview"),
+        prompt= "You are an fruit quality expert. Tell me tips for preventing fresh fruits from rotten. And effect of eating rotten fruits. Answer it in few lines only."
+        response = model.generate_content(prompt)
         Text = response.text
     except:
         return "Not any advice is currently available....."
@@ -15,5 +14,4 @@ def Gemini_ke_updesh():
         return "No advice available at this moment of time......"
     Text = re.sub(r"\*\*"  , "", Text)
     Text = re.sub(r"\n+" , " " , Text)
-
     return Text.replace("\\" , "")
